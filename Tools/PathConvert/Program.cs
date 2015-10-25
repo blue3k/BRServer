5869368ea39bbc2e0f38219a57f4bf71d4ec4803
blue3k <blue3k@gmail.com>
2015-10-25 00:05:56 -0700

    Working on linux build setting

---
+++ Tools/PathConvert/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BR.ToolLib;
+
+using System.IO;
+using System.Diagnostics;
+
+namespace shellbridge
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            ToolEnvironment.CheckDebugOption("pathconverter");
+
+            string path = ToolEnvironment.GetSettingString("path");
+            if (string.IsNullOrEmpty(path))
+            {
+                Console.WriteLine("Empty path paramater:");
+                return -1;
+            }
+
+            if (Path.DirectorySeparatorChar == '\\')
+                path = path.Replace('/', '\\');
+            else
+                path = path.Replace('\\', '/');
+
+            Console.WriteLine(path);
+
+            return 0;
+        }
+    }
+}
