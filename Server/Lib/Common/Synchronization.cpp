7cf135813a023a4875689c16722c73f2f4e13e04
blue3k <blue3k@gmail.com>
2015-10-28 19:00:41 -0700

    Add missed file

---
+++ Server/Lib/Common/Synchronization.cpp
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+// 
+// CopyRight (c) 2013 Madk
+// 
+// Author : KyungKun Ko
+//
+// Description : Thread library
+//	
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#include "stdafx.h"
+#include "Common/Trace.h"
+#include "Common/StrUtil.h"
+#include "Common/Thread.h"
+#include "Common/Synchronization.h"
+#include <dbghelp.h>
+#include <process.h>
+
+
+namespace BR
+{
+	template class TicketScopeLockT<TicketLock>;
+	template class TicketScopeLockT<FakeTicketLock>;
+
+
+
+
+	Ticketing::Ticket Ticketing::GetTotalWaitingCount() const
+	{
+		return m_Working.load(std::memory_order_relaxed) - m_Worked.load(std::memory_order_relaxed);
+	}
+
+
+
+
+}; // namespace BR
+
+
+
---
+++ Server/Lib/Common/SynchronizationTrait.h
@@ -0,0 +1,97 @@
+////////////////////////////////////////////////////////////////////////////////
+// 
+// CopyRight (c) 2013 MadK
+// 
+// Author : KyungKun Ko
+//
+// Description : Thread Synchronize
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+#pragma once
+
+#include "Common/Typedefs.h"
+#include "Common/BrAssert.h"
+#include "Common/Memory.h"
+#include "Common/Synchronization.h"
+#include "Common/SystemSynchronization.h"
+#include "Common/SortedMap.h"
+#include "Common/DualSortedMap.h"
+
+
+
+namespace BR
+{
+
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////
+	//
+	//	Synchronization trait
+	//
+
+	// Synchronization trait, not thread safe
+	struct ThreadSyncTraitNone
+	{
+		enum Consts {
+			ThreadSafe = 0
+		};
+
+		typedef FakeTicketLock	TicketLockType;
+	};
+
+	// Synchronization trait, thread safe
+	struct ThreadSyncTraitReadWrite
+	{
+		enum Consts {
+			ThreadSafe = 1
+		};
+		typedef TicketLock	TicketLockType;
+		//typedef SortedMap<class KeyType, class ValueType> BucketContainer;
+	};
+
+
+
+	template<class KeyType, class ValueType>
+	struct ThreadSyncTraitNoneT
+	{
+		enum Consts {
+			ThreadSafe = 0
+		};
+		typedef TicketLock	TicketLockType;
+		typedef SortedMap<KeyType, ValueType> BucketContainer;
+	};
+
+	template<class KeyType, class ValueType>
+	struct ThreadSyncTraitReadWriteT
+	{
+		enum Consts {
+			ThreadSafe = 1
+		};
+		typedef TicketLock	TicketLockType;
+		typedef DualSortedMap<KeyType, ValueType> BucketContainer;
+	};
+
+	// Synchronization trait, thread safe
+	template<class KeyType, class ValueType>
+	struct ThreadSyncTraitWriteT
+	{
+		enum Consts {
+			ThreadSafe = 1
+		};
+
+		typedef WriteOnlyTicketLock	TicketLockType;
+		typedef DualSortedMap<KeyType, ValueType> BucketContainer;
+	};
+
+
+
+
+
+
+}; // namespace BR
+
