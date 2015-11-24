037a3c075b1746d2cf8d1c81212e9faec553d332
blue3k <blue3k@gmail.com>
2015-11-24 03:18:01 -0800

    Add unit test for system system synchronization

---
+++ TestApp/SystemSynchronizeTest.cpp
@@ -0,0 +1,173 @@
+// TestApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <gtest/gtest.h>
+#include "Common/StrUtil.h"
+#include "Common/BRRandom.h"
+#include "Common/StrFormat.h"
+#include "Common/PageQueue.h"
+#include "Common/TimeUtil.h"
+#include "Common/CircularQueue.h"
+#include "Common/Thread.h"
+#include "Common/SharedPointer.h"
+#include "Common/SharedReferenceManager.h"
+#include "SystemSynchronizeTest.h"
+
+#include "Common/SystemSynchronization.h"
+
+
+using ::testing::EmptyTestEventListener;
+using ::testing::InitGoogleTest;
+using ::testing::Test;
+using ::testing::TestCase;
+using ::testing::TestEventListeners;
+using ::testing::TestInfo;
+using ::testing::TestPartResult;
+using ::testing::UnitTest;
+using namespace ::BR;
+
+
+
+#ifdef DEBUG
+#define TestScale 1
+#else
+#define TestScale 10
+#endif
+
+
+
+
+
+namespace BRTest
+{
+
+
+TEST_F(SystemSynchronizationTest, Mutex)
+{
+	const UINT64 TEST_LENGTH = TestScale * 999999;
+	const INT64 NUM_THREAD = 10;
+
+	SyncCounter workerCounter(0);
+	std::atomic<int> testData(0);
+	std::atomic<int> testIndex(0);
+	BR::Mutex dataLock;
+
+	for (UINT worker = 0; worker < NUM_THREAD; worker++)
+	{
+		auto pWorker = new FunctorThread([&dataLock, &testData, &testIndex, &workerCounter, worker, TEST_LENGTH](Thread* pThread)
+		{
+			workerCounter.fetch_add(1, std::memory_order_relaxed);
+			for (int iTest = 0; iTest < TEST_LENGTH; iTest++)
+			{
+				BR::MutexScopeLock scopeLock(dataLock);
+				auto read = testData.load(std::memory_order_acquire);
+				testData.store(read + 1, std::memory_order_release);
+				auto indexData = testIndex.fetch_add(1, std::memory_order_relaxed);
+				AssertRel(indexData == read);
+			}
+
+			workerCounter.fetch_sub(1, std::memory_order_relaxed);
+		});
+
+		pWorker->Start();
+		m_Threads.push_back(pWorker);
+	}
+
+	ThisThread::SleepFor(DurationMS(10));
+
+	// wait writer threads
+	while (workerCounter.load(std::memory_order_relaxed) > 0)
+	{
+		ThisThread::SleepFor(DurationMS(1000));
+	}
+}
+
+
+
+TEST_F(SystemSynchronizationTest, CriticalSection)
+{
+	const UINT64 TEST_LENGTH = TestScale * 999999;
+	const INT64 NUM_THREAD = 10;
+
+	SyncCounter workerCounter(0);
+	std::atomic<int> testData(0);
+	std::atomic<int> testIndex(0);
+	BR::CriticalSection dataLock;
+
+	for (UINT worker = 0; worker < NUM_THREAD; worker++)
+	{
+		auto pWorker = new FunctorThread([&dataLock, &testData, &testIndex, &workerCounter, worker, TEST_LENGTH](Thread* pThread)
+		{
+			workerCounter.fetch_add(1, std::memory_order_relaxed);
+			for (int iTest = 0; iTest < TEST_LENGTH; iTest++)
+			{
+				BR::MutexScopeLock scopeLock(dataLock);
+				auto read = testData.load(std::memory_order_acquire);
+				testData.store(read + 1, std::memory_order_release);
+				auto indexData = testIndex.fetch_add(1, std::memory_order_relaxed);
+				AssertRel(indexData == read);
+			}
+
+			workerCounter.fetch_sub(1, std::memory_order_relaxed);
+		});
+
+		pWorker->Start();
+		m_Threads.push_back(pWorker);
+	}
+
+	ThisThread::SleepFor(DurationMS(10));
+
+	// wait writer threads
+	while (workerCounter.load(std::memory_order_relaxed) > 0)
+	{
+		ThisThread::SleepFor(DurationMS(1000));
+	}
+}
+
+TEST_F(SystemSynchronizationTest, Event)
+{
+	const UINT64 TEST_LENGTH = TestScale * 999999;
+	const INT64 NUM_THREAD = 10;
+
+	SyncCounter workerCounter(0);
+	std::atomic<int> testData(0);
+	std::atomic<int> testIndex(0);
+	BR::Event dataEvent;
+
+	dataEvent.Reset();
+
+	for (UINT worker = 0; worker < NUM_THREAD; worker++)
+	{
+		auto pWorker = new FunctorThread([&dataEvent, &testData, &testIndex, &workerCounter, worker, TEST_LENGTH](Thread* pThread)
+		{
+			workerCounter.fetch_add(1, std::memory_order_relaxed);
+			for (int iTest = 0; iTest < TEST_LENGTH; iTest++)
+			{
+				dataEvent.WaitEvent(10000);
+				auto read = testData.load(std::memory_order_acquire);
+				testData.store(read + 1, std::memory_order_release);
+				auto indexData = testIndex.fetch_add(1, std::memory_order_relaxed);
+				AssertRel(indexData == read);
+			}
+
+			workerCounter.fetch_sub(1, std::memory_order_relaxed);
+		});
+
+		pWorker->Start();
+		m_Threads.push_back(pWorker);
+	}
+
+	ThisThread::SleepFor(DurationMS(10));
+
+	// wait writer threads
+	while (workerCounter.load(std::memory_order_relaxed) > 0)
+	{
+		ThisThread::SleepFor(DurationMS(1000));
+	}
+}
+
+
+
+}  // namespace BRTest
+
---
+++ TestApp/SystemSynchronizeTest.h
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////
+// 
+// CopyRight (c) 2013 The Braves
+// 
+// Author : KyungKun Ko
+//
+// Description : System Synchronization test
+//	
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#include "stdafx.h"
+#include <gtest/gtest.h>
+#include <ppl.h>
+#include "Common/Thread.h"
+#include "Common/MemoryPool.h"
+#include "TestBase.h"
+
+
+
+namespace BRTest
+{
+
+	class SystemSynchronizationTest : public MyTestBase
+	{
+	protected:
+
+		// Thread module test
+		std::vector<BR::Thread*>	m_Threads;
+
+		BR::SpinLock m_LockObject;
+
+	public:
+
+		void StopAllThread()
+		{
+			std::for_each( m_Threads.begin(), m_Threads.end(), []( BR::Thread* pThread )
+			{
+				if( pThread ) pThread->Stop( true );
+				delete pThread;
+			});
+			m_Threads.clear();
+		}
+
+		// Remember that SetUp() is run immediately before a test starts.
+		virtual void SetUp()
+		{
+			__super::SetUp();
+		}
+
+		// TearDown() is invoked immediately after a test finishes.  Here we
+		virtual void TearDown()
+		{
+			__super::TearDown();
+
+			StopAllThread();
+
+		}
+	};
+
+
+}  // namespace BRTest
+
+
