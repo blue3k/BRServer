2808afa516bc8b9087de8dd279a1bb71e71c10ce
blue3k <blue3k@gmail.com>
2016-03-20 01:13:54 -0700

    Fix service script

--- LinuxBuild/4_DistributeServers.sh
+++ LinuxBuild/4_DistributeServers.sh
@@ -11,7 +11,7 @@ export BUILD_MODE=$1
 
 scp -i ./LinuxBuild/Bravesprivate2.popen -r Server/bin/DEBUG/ blue3k@10.133.29.154:/GameServer/Server/bin/
 scp -i ./LinuxBuild/Bravesprivate2.popen -r Server/Config/ blue3k@10.133.29.154:/GameServer/Server/
-scp -i ./LinuxBuild/Bravesprivate2.popen Server/Config/ServerConfig_linux_rel.xml blue3k@10.133.29.154:/GameServer/Server/ServerConfig_linux.xml
+scp -i ./LinuxBuild/Bravesprivate2.popen Server/Config/ServerConfig_linux_rel.xml blue3k@10.133.29.154:/GameServer/Server/Config/ServerConfig_linux.xml
 
 
 
