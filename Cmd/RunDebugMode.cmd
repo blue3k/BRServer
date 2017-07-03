2474ea621248a27e58ad06ac710b1760a315ec23
blue3k <blue3k@gmail.com>
2017-07-03 02:31:17 -0700

    Config file name change for windows.

--- Cmd/RunDebugMode.cmd
+++ Cmd/RunDebugMode.cmd
@@ -2,5 +2,13 @@
 set COMMAND_ROOT=%~dp0
 
 set PATH_BIN=%COMMAND_ROOT%..\Server\bin\x64Debug
+set PATH_CONFIG=%COMMAND_ROOT%..\Server\Config
+
+if not exist %PATH_CONFIG%\ServerConfig.xml (
+    copy %PATH_CONFIG%\ServerConfig_winsample.xml %PATH_CONFIG%\ServerConfig.xml
+	call notepad %PATH_CONFIG%\ServerConfig.xml
+) else (
+    rem file doesn't exist
+)
 
 call _RunServers.cmd
