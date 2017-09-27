005f0494ec736d456a82f13125c4e0dd8433ba82
blue3k <blue3k@gmail.com>
2017-09-26 20:21:20 -0700

    Delete unused files

--- Cmd/RunDebugLogin.cmd
+++
@@ -1,9 +0,0 @@
-
-set COMMAND_ROOT=%~dp0
-
-set PATH_BIN=%COMMAND_ROOT%..\Server\bin\x64Debug
-
-del /s %PATH_BIN%\..\log\*.txt
-
-start %PATH_BIN%\BRModuleServer.Win.exe -debug -n:BRLoginServer01
-
