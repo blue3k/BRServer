bd97fb5bb6c6a82dbe40155098aa621f68eb4256
blue3k <blue3k@gmail.com>
2019-03-05 17:37:47 -0800

    Update script

--- Cmd/ServiceLauncherSyncRelease.cmd
+++ Cmd/ServiceLauncherSyncRelease.cmd
@@ -4,11 +4,10 @@ cd %~dp0
 
 set SERVICE_BIN_PATH=%~dp0..\Server\bin
 
-xcopy %SERVICE_BIN_PATH%\x64Release\BRServerInstanceLauncher.* %SERVICE_BIN_PATH% /Y /D
-xcopy %SERVICE_BIN_PATH%\x64Release\libcrypto-1_1-x64.* %SERVICE_BIN_PATH% /Y /D
-xcopy %SERVICE_BIN_PATH%\x64Release\libmysql.* %SERVICE_BIN_PATH% /Y /D
-xcopy %SERVICE_BIN_PATH%\x64Release\libssl-1_1-x64.* %SERVICE_BIN_PATH% /Y /D
-xcopy %SERVICE_BIN_PATH%\x64Release\zookeeper.* %SERVICE_BIN_PATH% /Y /D
+
+xcopy %SERVICE_BIN_PATH%\x64Release\*dll %SERVICE_BIN_PATH% /Y /D
+xcopy %SERVICE_BIN_PATH%\x64Release\*pdb %SERVICE_BIN_PATH% /Y /D
+xcopy %SERVICE_BIN_PATH%\x64Release\*exe %SERVICE_BIN_PATH% /Y /D
 
 
 
