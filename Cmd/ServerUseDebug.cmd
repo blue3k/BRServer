6a09f220d343f9f807c5dfd9fbf7f56716e425c1
blue3k <blue3k@gmail.com>
2019-11-28 01:58:30 -0800

    Add ServerUseDebug.cmd and ServerUseRelease.cmd

---
+++ Cmd/ServerUseDebug.cmd
@@ -0,0 +1,14 @@
+
+%~d0
+cd %~dp0
+
+set SERVICE_BIN_PATH=%~dp0..\Server\bin
+set SERVICE_BUILD_PTAH=%~dp0..\buildWindows\x64Debug\bin
+set SF_BIN_PTAH=%~dp0..\Server\bin\x64Debug
+
+robocopy %SERVICE_BUILD_PTAH%\ %SERVICE_BIN_PATH% *.dll *.exe *.pdb
+robocopy %SF_BIN_PTAH%\ %SERVICE_BIN_PATH% *.dll *.exe *.pdb
+
+
+
+pause
