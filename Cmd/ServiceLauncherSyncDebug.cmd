63d5a1cfc95fc60e85303947cc11e706663552bd
blue3k <blue3k@gmail.com>
2019-11-28 01:57:18 -0800

    Switch bin script with one script

--- Cmd/ServiceLauncherSyncDebug.cmd
+++ Cmd/ServiceLauncherSyncDebug.cmd
@@ -4,8 +4,10 @@ cd %~dp0
 
 set SERVICE_BIN_PATH=%~dp0..\Server\bin
 set SERVICE_BUILD_PTAH=%~dp0..\buildWindows\x64Debug\bin
+set SF_BIN_PTAH=%~dp0..\Server\bin\x64Debug
 
 robocopy %SERVICE_BUILD_PTAH%\ %SERVICE_BIN_PATH% *.dll *.exe *.pdb
+robocopy %SF_BIN_PTAH%\ %SERVICE_BIN_PATH% *.dll *.exe *.pdb
 
 
 
