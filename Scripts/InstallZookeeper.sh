4b7fb5f8a8a9264e5ddf1d09a031a3067fadde98
blue3k <blue3k@gmail.com>
2020-10-04 23:09:24 -0700

    zookeeper install script for linux

---
+++ Scripts/InstallZookeeper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+SF_PATH=$(dirname "$0")
+
+#download deb package bundle
+wget http://apache.mirrors.hoobly.com/zookeeper/zookeeper-3.6.2/apache-zookeeper-3.6.2-bin.tar.gz
+sudo mkdir /etc/zookeeper
+sudo tar -xvf apache-zookeeper-3.6.2-bin.tar.gz -C /etc/zookeeper --strip-components=1
+
+sudo mkdir /data
+sudo mkdir /data/zookeeper
+
+
+#install
+#sudo chown -R <user to run>:<your group> /data
+#sudo chown -R <user to run>:<your group> /etc/zookeeper
+
+sudo mkdir /etc/zookeeper/logs
+sudo chmod -R g+rw /etc/zookeeper/logs
+sudo chmod -R g+rw /data
+
+sudo cp /etc/zookeeper/conf/zoo_sample.cfg /etc/zookeeper/conf/zoo.cfg
+sudo vi /etc/zookeeper/conf/zoo.cfg
+
+sudo cp zookeeper.service /etc/systemd/system/zookeeper.service
+sudo vi /etc/systemd/system/zookeeper.service
+
+
+sudo systemctl start zookeeper
